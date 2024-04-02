9dd67f82c610205a6075bf4c81c6e59aa5902f8e
Tristian <tristianthedeveloper@gmail.com>
2024-04-02 12:25:45 -0700

    new stuff just got made!

--- src/fpstuff/Morphisms.java
+++ src/fpstuff/Morphisms.java
@@ -1,2 +1,34 @@
-package fpstuff;public class Morphisms {
+package fpstuff;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ *  The functions in here are pretty useless in most situations.
+ *  closure creation overhead means these might be slow enough so that their elegance isnt worth it.
+ */
+public class Morphisms {
+
+    /** Takes a function that takes two parameters, and returns a curried version of the function,
+     * A sequence of two functions each taking one argument.
+     *
+     * @param f The function to curry
+     * @return A curried version of the function.
+     */
+    public static <A, B, C> Function<A, Function<B, C>> curry(BiFunction<? super A, ? super B, ? extends C> f) {
+        return (A a) -> (B b) -> f.apply(a, b);
+    }
+
+    /** Uncurries a function.
+     * <p>
+     * think of it like this:
+     * <p>
+     * uncurry :: (a -> b -> c) -> (a, b) -> c
+     *
+     * @param f The function to uncurry
+     * @return An uncurried version of this function.
+     */
+    public static <A, B, C> BiFunction<A, B, C> uncurry(Function<A, Function<B, C>> f) {
+        return (a, b) -> f.apply(a).apply(b);
+    }
 }
--- src/fpstuff/MorphismsTests.java
+++ src/fpstuff/MorphismsTests.java
@@ -1,2 +1,22 @@
-package fpstuff;public class MorphismsTests {
+package fpstuff;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MorphismsTests {
+
+    @Test
+    void curry() {
+        Function<Integer, Function<Integer, Integer>> curried = Morphisms.curry(this::add);
+        int nine = curried.apply(4).apply(5);
+        Assertions.assertEquals(9, nine);
+        BiFunction<Integer, Integer, Integer> uncurried = Morphisms.uncurry(curried);
+        Assertions.assertEquals(uncurried.apply(4, 5), nine);
+    }
+
+    private int add(int x, int y) {
+        return x+y;
+    }
 }
